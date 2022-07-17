ccca0c93e0ec825868b4fcd3f1c8348be9ff8a2b
Mitch Lindgren <mitch@lindg.ren>
2022-07-17 16:08:59 -0700

    Added piss overlay

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Assertions;
 
 public class UIScript : MonoBehaviour
@@ -12,14 +13,19 @@ public class UIScript : MonoBehaviour
 
     public RectTransform StaminaMeterTransform;
     public RectTransform PissMeterTransform;
+    public Image PissOverlay;
 
     public TextMeshProUGUI RoundTimer;
 
     public TextMeshProUGUI ShoppingList;
     public TextMeshProUGUI RemainingQuota;
 
+    [Tooltip("Rate at which the piss overlay flashes when the piss limit is exceeded, seconds per flash.")]
+    public float PissOverlayPulseRate = 1.0f;
+
     private PlayerCharacterController _playerController;
     private Piss _playerPiss;
+    private bool _overlayShown = false;
 
     void UpdateShoppingList(Dictionary<PickableItemInfo, int> list)
     {
@@ -49,6 +55,23 @@ public class UIScript : MonoBehaviour
     {
         RemainingQuota.text = String.Format("Remaining quota: {0}", GameStateManager.Instance.OrdersRemainingForDailyQuota);
     }
+
+    IEnumerator PulsePissOverlay()
+    {
+        _overlayShown = true;
+        float startTime = Time.time;
+
+        while(_playerPiss.PissOverloadTimer > 0)
+        {
+            float deltaTime = Time.time - startTime;
+            float overlayOpacity = (float) Math.Abs(Math.Sin(deltaTime * Math.PI));
+            PissOverlay.color = new Color(1f, 1f, 1f, overlayOpacity);
+            yield return null;
+        }
+
+        PissOverlay.color = new Color(1f, 1f, 1f, 0);
+        _overlayShown = false;
+    }
     
     void Awake()
     {
@@ -70,6 +93,11 @@ public class UIScript : MonoBehaviour
 
         float pissRatio = Math.Min(_playerPiss.CurrentPiss / _playerPiss.MaxPiss, 1f);
         PissMeterTransform.localScale = new Vector3(pissRatio, 1f, 1f);
+
+        if(_playerPiss.PissOverloadTimer > 0 && !_overlayShown)
+        {
+            StartCoroutine(PulsePissOverlay());
+        }
 
         TimeSpan t = TimeSpan.FromSeconds(GameStateManager.Instance.RemainingTimeInRound);
         RoundTimer.text = String.Format("{0:D2}:{1:D2}", t.Minutes, t.Seconds);
